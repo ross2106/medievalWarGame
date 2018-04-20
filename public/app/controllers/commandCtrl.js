09cecf3c7f94aba112d2acd893a5c0d3f2ad6f6b
ROSS HENDERSON <ross1994@hotmail.co.uk>
2018-04-20 15:16:39 +0100

    make it better

--- public/app/controllers/commandCtrl.js
+++ public/app/controllers/commandCtrl.js
@@ -2,7 +2,9 @@ angular.module('commandCtrl', [])
     .controller('commandController', function ($scope, Socket, Auth) {
         Socket.connect();
         $scope.commands = [];
+        var Inventory = require('../models/inventory');
         var username = '';
+
         var getUsername = function () {
             Auth.getUser()
                 .then(function (response) {
@@ -11,16 +13,43 @@ angular.module('commandCtrl', [])
         };
         getUsername();
 
+        var addGold = function(){
+            Inventory.findOne({
+                'username': getUsername()
+            }, function (err, user) {
+                if(user){
+                    var query = {
+                        gold: gold+(Math.floor(Math.random() * (100+1)))
+                    };
+                    Inventory.update(query, options, callback);
+                } else{
+                    var newInventory = new Inventory();
+                    newInventory.username = getUsername();
+                    newInventory.gold = (Math.floor(Math.random() * (100+1)));
+                    newInventory.gold = 0;
+                    newInventory.gold = 0;
+                    newInventory.save();
+                }
+            });
+        };
+        var addWood = function(){
+
+        };
+        var addFood = function(){
+
+        };
+
         $scope.sendCommand = function (cmd) {
             switch (cmd) {
                 case 'mine_gold':
                     Socket.emit('command', {command: cmd});
+                    addGold();
                     break;
                 case 'chop_wood':
-                    console.log('chop_wood');
+                    Socket.emit('command', {command: cmd});
                     break;
                 case 'gather_food':
-                    console.log('gather_food');
+                    Socket.emit('command', {command: cmd});
                     break;
             }
             $scope.cmd = '';

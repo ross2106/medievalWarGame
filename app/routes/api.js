cb3e5704514e75fe975d8f8e5824e4a9c95da988
ROSS HENDERSON <ross1994@hotmail.co.uk>
2018-04-21 14:19:36 +0100

    make it better

--- app/routes/api.js
+++ app/routes/api.js
@@ -168,7 +168,13 @@ module.exports = function (app, express) {
                 //return the inventory
                 res.json(inventory);
                 // save the inventory
-                inventory.save();
+                inventory.save(function (err) {
+                    if (err) res.send(err);
+
+                    // return a message
+                    res.json({
+                        message: 'Inventory updated!'
+                    });
             })
         })
 

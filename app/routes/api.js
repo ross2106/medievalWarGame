d6a11b23294dfff47642e5b52f1bbc7025118b08
ROSS HENDERSON <ross1994@hotmail.co.uk>
2018-04-21 12:20:01 +0100

    make it better

--- app/routes/api.js
+++ app/routes/api.js
@@ -154,7 +154,7 @@ module.exports = function (app, express) {
                 if (err) res.send(err);
 
                 // return the inventories
-                res.json(inventories)
+                res.send(req.decoded)
             });
         });
 

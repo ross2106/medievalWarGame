27e361127f3129dd145f159e3a748b6d44b7ce43
ROSS HENDERSON <ross1994@hotmail.co.uk>
2018-04-23 10:48:58 +0100

    make it better

--- public/app/controllers/battleCtrl.js
+++ public/app/controllers/battleCtrl.js
@@ -110,7 +110,7 @@ angular.module('battleCtrl', [])
         //When somebody is challenged, get the details about the person being challenged
         $scope.getChallenger = function (index) {
             vm.getAllArmies();
-            while (gettingArmies) {
+            while (!gettingArmies) {
                 console.log(vm.armies);
                 $scope.challenge = index; //The person who is being challenged
                 vm.challengeUser = $scope.users[$scope.challenge];
